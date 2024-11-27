31d852b6406ea9610b898cdc90f3396ac0f81ca6
iCollegeHK <alextam@icollege.hk>
2024-11-27 19:57:14 +0800

    ed by IJ

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!000000000100001");
+        System.out.println("000010010010");
     }
 }
